de445247b2679f01c0338d0de49905b24d4d1bc5
Bjørn Birk Hansen <bjornbirk@Bjrns-MacBook-Pro.local>
2022-05-10 13:29:22 +0200

    firestore add data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -7,11 +8,45 @@ import { DataService } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor(private dataService: DataService) {
+  notes = []; 
+  constructor(private dataService: DataService, private alertCtrl: AlertController) {
     this.dataService.getNotes().subscribe(res => {
       console.log(res);
+      this.notes = res; 
     })
+  }
+
+  openNote(note) {}
+
+  async addNote() {
+    const alert = await this.alertCtrl.create({
+      header: 'Add Note',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'my note title',
+          type: 'text'
+        },
+        {
+          name: 'text',
+          placeholder: 'my note text',
+          type: 'textarea'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel,'
+      },
+      {
+        text: 'Add',
+        handler: (res) => {
+          this.dataService.addNote({title: res.title, text: res.text})
+        }
+      }
+    ]
+    });
+    await alert.present();
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,14 +1,28 @@
 import { Injectable } from "@angular/core";
-import { Firestore, collectionData } from "@angular/fire/firestore";
-import {collection} from '@firebase/firestore'
+import { Firestore, collection, collectionData, doc, docData, addDoc } from "@angular/fire/firestore";
+// import {collection} from '@firebase/firestore'
+import { Observable } from "rxjs";
+
+export interface Note {
+    id?: string;
+    title: string;
+    text: string; 
+}
+
+
 @Injectable({
     providedIn: 'root'
 })
 export class DataService {
     constructor(private firestore: Firestore) { }
     
-    getNotes() {
+    getNotes(): Observable<Note[]> {
         const notesRef = collection(this.firestore, 'notes');
-        return collectionData(notesRef);
+        return collectionData(notesRef,{ idField: 'id'} ) as Observable<Note[]>;
+    }
+
+    addNote(note: Note) {
+        const notesRef = collection(this.firestore, 'notes');
+        return addDoc(notesRef, note);
     }
 }
